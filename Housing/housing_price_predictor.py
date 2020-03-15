3d8cd3bffca783a4483f504a7e6137eab472b5d6
sr198 <shreejan@gmail.com>
2020-03-15 16:49:20 -0400

    scatter

--- Housing/housing_price_predictor.py
+++ Housing/housing_price_predictor.py
@@ -4,6 +4,7 @@ import tarfile
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix 
 
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -25,7 +26,7 @@ def extract_housing_csv( housing_path ):
 #Read csv file and load the data
 def load_housing_data_from_csv( housing_path ):
     csv_file = os.path.join( housing_path, "housing.csv" )
-    #return a pandas data frame object
+    #return a pandas DataFrame object
     return pd.read_csv( csv_file )
 
 def analyze_data_frame( data_frame:pd.DataFrame ):
@@ -39,7 +40,6 @@ def analyze_data_frame( data_frame:pd.DataFrame ):
 #Plot some graphs from the data frame
 def graph_my_data( data_frame ):
     data_frame.hist( bins=50, figsize=(20,15))
-    plt.show()
 
 #Split data into training/test data 
 def simple_split_traing_test_set( data_frame:pd.DataFrame, test_ratio ):
@@ -47,7 +47,7 @@ def simple_split_traing_test_set( data_frame:pd.DataFrame, test_ratio ):
     test_set_size = int( len(data_frame) * test_ratio )
     test_indices = shuffled_indices[:test_set_size]
     train_incides = shuffled_indices[test_set_size:]
-    return data_frame.iloc[train_incides], data_frame.iloc[test_indices]
+    return data_frame.iloc[train_indices], data_frame.iloc[test_indices]
 
 #We will use scikit-learn's train_test_split method
 def split_training_test_set( data_frame ):
@@ -61,6 +61,11 @@ def stratified_split_training_test_set( data_frame ):
         strat_train_set = data_frame.loc[train_index]
         strat_test_set = data_frame.loc[test_index]
     return strat_train_set, strat_test_set
+
+#correlation
+def draw_correlation_matrix( data_frame ):
+    correlation_attributes=["median_house_value", "median_income", "total_rooms", "housing_median_age"]
+    scatter_matrix( data_frame[correlation_attributes], figsize=(12,8), alpha=0.1 )
 #Cleanup
 
 #Apply ML algorithms
@@ -76,11 +81,9 @@ analyze_data_frame( housing_data_frame )
 graph_my_data( housing_data_frame )
 strat_train_set, strat_test_set = stratified_split_training_test_set( housing_data_frame )
 train_set = strat_train_set.copy()
-print( train_set )
 train_set.plot( kind="scatter", x="longitude", y="latitude" )
 train_set.plot( kind="scatter", x="longitude", y="latitude",alpha=0.1,
 s=train_set["population"]/100, label="population", figsize=(10,7),
 c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True )
-train_set.plot( kind="scatter", x="longitude", y="latitude",alpha=0.1)
-plt.legend()
+draw_correlation_matrix( train_set )
 plt.show()
